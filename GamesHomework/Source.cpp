c6c7bee6c14dee895303e2659dddae4255e058e8
MaxGuidry <mguidry7@outlook.com>
2016-10-15 13:09:53 -0500

    more tests

--- GamesHomework/Source.cpp
+++ GamesHomework/Source.cpp
@@ -6,7 +6,7 @@
 int main()
 {
 	srand(time(NULL));
-	PlaySound(TEXT("mus_combat_boss_02.wav"), NULL, SND_FILENAME | SND_ASYNC);
+	PlaySound(TEXT("mus_combat_01.wav"), NULL, SND_FILENAME | SND_ASYNC);
 	Application* CurrentGame=new RPS(false);
 	CurrentGame->Start();
 	CurrentGame->Run();
