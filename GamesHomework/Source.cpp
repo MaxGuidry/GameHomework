d6997f07ca61ccb65356b679506ce7c65884525f
MaxGuidry <mguidry7@outlook.com>
2016-10-14 19:38:48 -0500

    test

--- GamesHomework/Source.cpp
+++ GamesHomework/Source.cpp
@@ -3,9 +3,9 @@
 #include<Windows.h>
 #include<string>
 #include "Header.h"
-
 int main()
 {
+	PlaySound(TEXT("mus_combat_boss_02.wav"), NULL, SND_FILENAME | SND_ASYNC);
 	Application* CurrentGame=new RPS(false);
 	CurrentGame->Start();
 	CurrentGame->Run();
